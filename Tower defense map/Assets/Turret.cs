bca93bc0ddf1162159ba56156e2acc5d97c7fad0
LukeWall <Wallluke06@gmail.com>
2023-12-05 09:17:50 -0500

    .

---
+++ Tower defense map/Assets/Turret.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    private Transform target;
+    public float range;
+    public Transform partToRotate;
+    public float turnSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        float shortestDistance = Mathf.Infinity;
+        GameObject neareastEnemy = null;
+        foreach(GameObject Enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, Enemy.transform.position);
+            if(distanceToEnemy <= shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                neareastEnemy = Enemy;
+            }
+            if (neareastEnemy != null && shortestDistance <=range)
+            {
+                target = neareastEnemy.transform;
+            }
+            else
+            {
+                target = null;
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            partToRotate.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        
+
+        //target lock on
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
