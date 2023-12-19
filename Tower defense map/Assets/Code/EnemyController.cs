b21802e571e5f57c13b4b7502872f559b01b2038
LukeWall <Wallluke06@gmail.com>
2023-12-19 10:48:52 -0500

    .

--- Tower defense map/Assets/Code/EnemyController.cs
+++ Tower defense map/Assets/Code/EnemyController.cs
@@ -10,19 +10,21 @@ public class EnemyController : MonoBehaviour
     private Transform target;
     
     public int wavepointIndex = 0;
-   
+    Waypoints waypoint;
    
      void Start()
     {
         target = Waypoints.points[0];
         enemyStats = GetComponent<EnemyStats>();
-
+        waypoint = GetComponent<Waypoints>();
     }
 
      void Update()
     {
         Vector3 dir = target.position - transform.position;
+        Quaternion rot = target.rotation;
         transform.Translate(dir.normalized  * enemyStats.startSpeed * Time.deltaTime, Space.World);
+        transform.LookAt(target);
         if (Vector3.Distance(transform.position, target.position) <= 0.2f)
         {
             GetNextWaypoint();
